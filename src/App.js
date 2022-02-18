44155b83a6e0306984583f5a85c5e06096f5e43a
Max <max.edison@yahoo.com>
2022-02-18 13:05:29 -0500

    personalization

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, World!
+You found my site! Thanks for your patience while I get it up and running.
         </p>
         <a
           className="App-link"
