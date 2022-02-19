718f1043c783cc052cd497a57bbacf737bdb6320
Max <max.edison@yahoo.com>
2022-02-18 23:40:58 -0500

    webdeveloper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!<br />
-          Hello, World!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <p>
+      w<br />
+      e<br />
+      b<br />
+      d<br />
+      e<br />
+      v<br />
+      e<br />
+      l<br />
+      o<br />
+      p<br />
+      e<br />
+      r<br />
+      </p>
     </div>
   );
 }
