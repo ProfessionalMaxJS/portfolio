5af76db8edbf4da39a35d1dad1521b1fc3d162bd
Max <max.edison@yahoo.com>
2022-02-23 16:15:28 -0500

    configuring display

--- src/WebExperience.js
+++ src/WebExperience.js
@@ -6,16 +6,29 @@ function WebExperience(){
 
     return(
         <>
-
-      <a href="https://youtu.be/i81w1Ufl2RM" target="_blank" rel="noreferrer noopener">
-      <img className="blue" style={{width:"25%", height:"auto"}} src={dtdj} alt="Death to Dad Jokes demo" onClick={()=>console.log("elloGuvnah1")}/>
-      </a><br />
-      <a href="https://youtu.be/Q_tBBrbX8oY" target="_blank" rel="noreferrer noopener">
-      <img className="blue" style={{width:"25%", height:"auto"}} src={chakra} alt="Death to Dad Jokes demo" onClick={()=>console.log("elloGuvnah2")}/>
-      </a><br />
-      <a href="https://youtu.be/Mw1BVkK5coM" target="_blank" rel="noreferrer noopener">
-      <img className="blue" style={{width:"25%", height:"auto"}} src={reciPlenty} alt="Death to Dad Jokes demo" onClick={()=>console.log("elloGuvnah3")}/>
+        <h3 >ReciPlenty</h3>
+        <div className="blue">
+        <a href="https://youtu.be/Mw1BVkK5coM" target="_blank" rel="noreferrer noopener">
+      <img src={reciPlenty} alt="ReciPlenty demo" />
       </a>
+      <p>Test</p>
+      </div>
+
+      <h3>Chakra Cafe</h3>
+      <div className="blue">
+      <a href="https://youtu.be/Q_tBBrbX8oY" target="_blank" rel="noreferrer noopener">
+      <img src={chakra} alt="Chakra Cafe demo" />
+      </a>
+      <p>Test</p>
+      </div>
+
+      <h3>Death to Dad Jokes </h3>
+      <div className="blue">
+      <a href="https://youtu.be/i81w1Ufl2RM" target="_blank" rel="noreferrer noopener">
+      <img src={dtdj} alt="Death to Dad Jokes demo" />
+      </a>
+      <p>Test</p>
+    </div>
         </>
     )
 }
