fd9f5813c0481ee6901c01aca2942c5b5c4d4f5b
Max <max.edison@yahoo.com>
2022-03-01 17:50:24 -0500

    we're getting there

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import AssociatedExperience from './AssociatedExperience'
 import WebExperience from './WebExperience'
 import HomeScreen from './HomeScreen'
 import {useState} from 'react'
-import { Route, Routes, Link} from 'react-router-dom'
+import { useNavigate, Route, Routes} from 'react-router-dom'
 
 
 function App() {
 
+  const paginate = useNavigate()
   const [home, setHome] = useState(false)
   const [web, setWeb] = useState(false)
   const [assoc, setAssoc] = useState(false)
@@ -22,38 +23,49 @@ function App() {
 </Routes>
 
 
-    <div id="corner" style={{display: "flex"}}>
-    <div id="webdeveloper" style={{display: "flex", flexDirection: "column"}} >
-    <div style={{display: "flex"}} >
+    <div id="corner" style={{position:"fixed", bottom: "0px", left: "0px", display: "flex", color: "black"}}>
+    <div id="webdeveloper" style={{display: "flex", flexDirection: "column", backgroundColor:"#D3D3D3" /*, maxWidth: "10%"*/}} >
+    
+    <div id="web" style={{display: "flex"}} >
     <div className="hovee">w</div>
     <div className="hovee">e</div>
-    <div className="hovee">b</div>
+    <div className="hovee" id="bio" onClick={()=>paginate("/")} onMouseOver={()=>setHome(true)} onMouseLeave={()=>setHome(false)} >b</div>
     </div>
-    <div style={{display: "flex"}}>
+   
+    <div id="deve" style={{display: "flex"}}>
     <div className="hovee">d</div>
     <div className="hovee">e</div>
     <div className="hovee">v</div>
-    <div className="hovee">e</div>
-    <div className="hovee">l</div>
+    <div className="hovee" id="experience" onClick={()=>paginate("/WebExperience")} onMouseOver={()=>setWeb(true)} onMouseLeave={()=>setWeb(false)} >e</div>
     </div>
-    <div style={{display: "flex"}}>    
+    <div id="loper" style={{display: "flex"}}>    
+    <div className="hovee">l</div>
     <div className="hovee">o</div>
     <div className="hovee">p</div>
     <div className="hovee">e</div>
-    <div className="hovee">r</div>
+    <div className="hovee" id="resume" onClick={()=>paginate("/AssociatedExperience")} onMouseOver={()=>setAssoc(true)} onMouseLeave={()=>setAssoc(false)} >r</div>
     </div>
     </div>
-    <div id="lines" style={{ display: "flex", flexDirection: "column" }} >
+
+    <div id="links" style={{ display: "flex", alignItems: "stretch", flexDirection: "column", backgroundColor:"#D3D3D3"}} >
+  
+  {home && <div id="home" style={{display: "flex"}}>
     <hr style={{ transform:"skewY(-4deg)", width:"100px"}} />
-    <hr style={{width:"100px"}} />
-    <hr style={{width:"100px"}} />
-    <hr style={{width:"100px"}} />
+    <div><div className='white' to="/">Bio / Contact</div></div>
+ </div>}
+    
+  {web && <div style={{display:"flex"}} >
+    <hr style={{transform:"skewY(-22deg)", width:"100px"}} />
+    <div> <div className='white' to="/WebExperience" >Experience / Projects</div></div>
+    </div>}
+
+  {assoc && <div style={{display:"flex"}} >
+    <hr style={{width:"100px", transform:"skewY(-40deg)"}} />
+    <div><div className='white' to="/AssociatedExperience">Resume / Other Cool Things</div></div>
+    </div>}
+
     </div>
-    <div id="links" style={{display: "flex", flexDirection: "column"}} >
-    <div><Link className='white' to="/">Home</Link></div>
-    <div><Link className='white' to="/WebExperience" >Web Experience</Link></div>
-    <div><Link className='white' to="/AssociatedExperience">Associated Experience</Link></div>
-    </div>
+
     </div>
 
     </div>
