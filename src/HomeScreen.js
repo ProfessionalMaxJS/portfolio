1be6ec3986e3b26d17a2c09be32fcad463e5d7db
Max Engel-Streich <90570581+ProfessionalMaxJS@users.noreply.github.com>
2023-02-25 21:03:22 -0500

    Update HomeScreen.js

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -21,6 +21,7 @@ function HomeScreen(){
         <a className="white" target="_blank" rel="noreferrer noopener" href="http://www.maxees.blogspot.com" >Blog</a>
         <a className="white" target="_blank" rel="noreferrer noopener" href="https://www.GitHub.com/ProfessionalMaxJS" >GitHub</a>
         <a className="white" target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/maxees/" >LinkedIn</a>
+        <a className="white" target="_blank" rel="noreferrer noopener" href="https://tinyurl.com/max-beerclass" >Take my Beer Brewing Class!</a>
     {/* </div> 
         <div className="inLinks" > */}
         <a className="white" target="_blank" rel="noreferrer noopener" href="mailto:max@maxees.dev?subject=You're Hired!" >Max@MaxEES.dev</a>
